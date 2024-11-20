532c6b62e62c450266c293f8f6d7a0a432f522f1
Rachel Terry <rachel.terry@earthscope.org>
2024-11-20 11:55:44 -0700

    update download list

--- src/es_sfgtools/processing/pipeline/data_handler.py
+++ src/es_sfgtools/processing/pipeline/data_handler.py
@@ -317,12 +317,12 @@ class DataHandler:
                         if not file_asset.local_path.exists():
                             assets_to_download.append(file_asset)
 
-            if len(assets_to_download) == 0:
-                logger.info(f"No new files to download")
-            
-            # split the entries into s3 and http
-            s3_assets = [file for file in assets if file.remote_type == REMOTE_TYPE.S3.value]
-            http_assets = [file for file in assets if file.remote_type == REMOTE_TYPE.HTTP.value]
+        if len(assets_to_download) == 0:
+            logger.info(f"No new files to download")
+        
+        # split the entries into s3 and http
+        s3_assets = [file for file in assets_to_download if file.remote_type == REMOTE_TYPE.S3.value]
+        http_assets = [file for file in assets_to_download if file.remote_type == REMOTE_TYPE.HTTP.value]
 
             # Download Files from either S3 or HTTP
             if len(s3_assets) > 0:
