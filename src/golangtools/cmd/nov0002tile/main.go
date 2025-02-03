f24424fd2915db6b5521c8c5ee5b070f0284a754
Franklyn Dunbar <franklyn.dunbar@earthscope.org>
2025-02-03 16:44:49 -0700

    transfer and build golang utils locally

---
+++ src/golangtools/cmd/nov0002tile/main.go
@@ -0,0 +1,299 @@
+// Author: Franklyn Dunbar  | Contact franklyn.dunbar@earthscope.org | Dec 2024
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+
+	tiledb "github.com/TileDB-Inc/TileDB-Go"
+	log "github.com/sirupsen/logrus"
+	"gitlab.com/earthscope/gnsstools/pkg/common/gnss/observation"
+	novatelascii "gitlab.com/earthscope/gnsstools/pkg/encoding/novatel/novatel_ascii"
+	"gitlab.com/earthscope/gnsstools/pkg/encoding/tiledbgnss"
+)
+
+func removeBeforeASCIISyncChar(s string) (string, error) {
+	longMessageIndex := strings.Index(s, "#")
+	shortMessageIndex := strings.Index(s, "%")
+	switch {
+	case longMessageIndex != -1:
+		return s[longMessageIndex:], nil
+	case shortMessageIndex != -1:
+		return s[shortMessageIndex:], nil
+	default:
+		return "", fmt.Errorf("novatel ASCII sync char not found")
+	}
+}
+
+
+func processBuffer(buffer []byte) (message novatelascii.Message, err error) {
+	stringArray := string(buffer)
+	trimmedLine,err := removeBeforeASCIISyncChar(stringArray)
+	if err != nil {
+		return message, err
+	}
+	//fmt.Print("\n Trimmed Line: ", trimmedLine)
+	endOfHeaderIndex := strings.Index(trimmedLine, ";")
+	endOfDataIndex := strings.Index(trimmedLine, "*")
+
+	if endOfDataIndex <= endOfHeaderIndex {
+		return message, fmt.Errorf("message is missing checksum")
+		// endOfDataIndex = len(trimmedLine) - 1
+	}
+	if endOfDataIndex == -1 {
+		return message, fmt.Errorf("message is missing checksum")
+		// endOfDataIndex = len(trimmedLine) - 1
+	}
+	if endOfHeaderIndex< 2 {
+		return message, fmt.Errorf("message is too short")
+	}
+	splitHeaderText := strings.Split(trimmedLine[1:endOfHeaderIndex], ",")
+	if len(splitHeaderText) < 10 {
+		return message, fmt.Errorf("message header is too short")
+	}
+	switch trimmedLine[0] {
+		case '#': // long
+			sequence, err := strconv.Atoi(splitHeaderText[2])
+			if err != nil {
+				return message, err
+			}
+			idleTime, err := strconv.ParseFloat(splitHeaderText[3], 64)
+			if err != nil {
+				return message, err
+			}
+			week, err := strconv.ParseFloat(splitHeaderText[5], 64)
+			if err != nil {
+				return message, err
+			}
+			seconds, err := strconv.ParseFloat(splitHeaderText[6], 64)
+			if err != nil {
+				return message, err
+			}
+			recStatus, err := strconv.ParseFloat(splitHeaderText[7], 64)
+			if err != nil {
+				return message, err
+			}
+			recSWVersion, err := strconv.ParseFloat(splitHeaderText[9], 64)
+			if err != nil {
+				return message, err
+			}
+			longMessage := novatelascii.LongMessage{
+				Sync:         string(trimmedLine[0]),
+				Msg:          splitHeaderText[0],
+				Port:         splitHeaderText[1],
+				Sequence:     sequence,
+				IdleTime:     idleTime,
+				TimeStatus:   splitHeaderText[4],
+				Week:         week,
+				Seconds:      seconds,
+				RecStatus:    recStatus,
+				Reserved:     splitHeaderText[8],
+				RecSWVersion: recSWVersion,
+				Data:         trimmedLine[endOfHeaderIndex+1 : endOfDataIndex],
+				Checksum:     trimmedLine[endOfDataIndex:],
+			}
+			return longMessage, nil
+	default:
+		return novatelascii.LongMessage{}, fmt.Errorf("unknown error")
+	}
+
+}
+
+	
+func DeserializeNOV00bin(r *bufio.Reader) (message novatelascii.Message, err error) {
+	var stx byte = 0x2 // start of text, 2 in decimal
+	var etx byte = 0x3 // end of text, 3 in decimal
+	var log_start byte = 0x23 // log start, 35 in decimal ASCII #
+	var log_done byte = 0x2A// log done, 2 in decimal, * in Ascii
+	var got_start_of_text bool = false
+	var got_end_of_text bool = false
+	var got_start_of_log bool = false
+	var got_end_of_log bool = false
+	var buffer []byte
+
+	for {
+		peekByte, err := r.Peek(1)
+		if err != nil {
+			switch {
+			case err == io.EOF || err == bufio.ErrBufferFull:
+				// do not advance the reader
+				return message, err
+			default:
+				// advance the reader
+				_, err := r.Discard(1)
+				if err != nil {
+					log.Warnf("error discarding byte (%s)", err)
+				}
+				return message, fmt.Errorf("error peeking byte (%s)", err)
+			}
+		}
+		if peekByte[0] == stx {
+			got_start_of_text = true
+		} else if peekByte[0] == log_start {
+			got_start_of_log = true
+			buffer = []byte{}
+		} else if peekByte[0] == etx{
+			got_end_of_text = true
+		} else if peekByte[0] == log_done {
+			got_end_of_log = true
+			got_end_of_text = false
+		}
+		if got_end_of_text && got_end_of_log {
+			buffer = append(buffer, peekByte[0])
+			_, err := r.Discard(1)
+			if err != nil {
+				log.Warnf("error discarding byte (%s)", err)
+			}
+			message, err := processBuffer(buffer)
+			if err != nil {
+				break
+			}
+			return message, err
+		} else if got_start_of_text && got_start_of_log{
+			buffer = append(buffer, peekByte[0])
+		}
+		_, err = r.Discard(1)
+		if err != nil {
+			log.Warnf("error discarding byte (%s)", err)
+		}
+	}
+	
+
+	return novatelascii.LongMessage{}, fmt.Errorf("unknown error")
+
+}
+	
+type Reader struct {
+	Reader *bufio.Reader
+}
+
+func NewReader(r io.Reader) Reader {
+	return Reader{Reader: bufio.NewReader(r)}
+}
+
+func (reader Reader) NextMessage() (message novatelascii.Message, err error) {
+	message,err = DeserializeNOV00bin(reader.Reader)
+	if err != nil {
+		if err == io.EOF {
+			return message, err
+		}
+	}
+	return message, nil
+}
+
+// processFileNOV000 reads a file containing GNSS data, processes it, and returns a slice of observation.Epoch.
+// It opens the specified file, reads messages using a custom reader, and processes "RANGEA" messages
+// to extract GNSS epoch data. The function handles errors appropriately and ensures the file is closed
+// after processing.
+//
+// Parameters:
+//   - file: The path to the file to be processed.
+//
+// Returns:
+//   - A slice of observation.Epoch containing the processed GNSS epoch data.
+func processFileNOV000(file string) []observation.Epoch{
+	f,err := os.Open(file)
+	if err != nil {
+		log.Fatalf("failed opening file %s, %s ",file, err)
+	}
+	defer f.Close()
+	reader := NewReader(bufio.NewReader(f))
+	epochs := []observation.Epoch{}
+
+	epochLoop:
+		for {
+			message,err := reader.NextMessage()
+			if err != nil {
+				if err == io.EOF {
+					err = f.Close()
+					if err != nil {
+						log.Errorln(err)
+					}
+					break epochLoop
+				}
+				log.Println(err)
+			}
+			switch m:=message.(type) {
+				case novatelascii.LongMessage:
+					if m.Msg == "RANGEA" {
+						rangea, err := novatelascii.DeserializeRANGEA(m.Data)
+						if err != nil {
+							continue
+						}
+						epoch, err := rangea.SerializeGNSSEpoch(m.Time())
+						if err != nil {
+							continue
+						}
+						epochs = append(epochs, epoch)
+					}
+				}
+		}
+	return epochs
+}	
+
+func main() {
+	tdbPathPtr := flag.String("tdb", "", "Path to the TileDB array")
+	numProcsPtr := flag.Int("procs", 10, "Number of concurrent processes")
+	flag.Parse()
+	filenames := flag.Args()
+	if len(filenames) == 0 {
+		flag.PrintDefaults()
+		log.Fatalln("no files specified")
+	}
+	if !ArrayExists(*tdbPathPtr) {
+		err := tiledbgnss.CreateArray("s3://earthscope-tiledb-schema-dev-us-east-2-ebamji/GNSS_OBS_SCHEMA_V3.tdb/", *tdbPathPtr, "us-east-2")
+		if err != nil {
+			log.Errorf("error creating array: %v",err)
+		}
+	} else {
+		log.Infof("array %s already exists", *tdbPathPtr)
+	}
+	var wg sync.WaitGroup
+	sem := make(chan struct{}, *numProcsPtr) // Limit to 10 concurrent goroutines
+	for _, filename := range filenames {
+		wg.Add(1)
+		go func(filename string) {
+			defer wg.Done()
+			sem <- struct{}{}
+			defer func() { <-sem }()
+			epochs := processFileNOV000(filename)
+			if len(epochs) == 0 {
+				log.Warnf("no epochs found in file %s", filename)
+				return
+			}
+			log.Infof("processed %d epochs from file %s", len(epochs), filename)
+			err := tiledbgnss.WriteObsV3Array( *tdbPathPtr,"us-east-2",epochs)
+			if err != nil {
+				log.Errorf("error writing epochs to array: %v",err)
+			}
+		}(filename)
+	
+	}
+	wg.Wait()
+}
+
+// Check if the TileDB array exists at the given path
+func ArrayExists(arrayPath string) bool {
+	ctx, err := tiledb.NewContext(nil)
+	if err != nil {
+		log.Errorf("failed creating TileDB context: %v", err)
+		return false
+	}
+	defer ctx.Free()
+
+	schema, err := tiledb.LoadArraySchema(ctx, arrayPath)
+	if err != nil{
+		log.Errorf("failed to load TileDB array schema: %v",err)
+	}
+	if schema == nil {
+		return false
+	} else {
+		return true
+	}
+}
--- src/golangtools/cmd/tdb2rnx/main.go
+++ src/golangtools/cmd/tdb2rnx/main.go
@@ -6,9 +6,12 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sort"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 
+	"gitlab.com/earthscope/gnsstools/pkg/common/gnss/observation"
 	"gitlab.com/earthscope/gnsstools/pkg/encoding/rinex"
 	"gitlab.com/earthscope/gnsstools/pkg/encoding/tiledbgnss"
 )
@@ -46,92 +49,92 @@ func main() {
 	fmt.Print(timeStart)
 	fmt.Print(timeEnd)
 
-	// timeSlices := tiledbgnss.GetDateArranged(timeStart,timeEnd)
+	timeSlices := tiledbgnss.GetDateArranged(timeStart,timeEnd)
 
-	// fmt.Print("\n Time Slices \n")
-	// for _,timeSlice := range timeSlices {
-	// 	// Clear the epochs slice
+	fmt.Print("\n Time Slices \n")
+	for _,timeSlice := range timeSlices {
+		// Clear the epochs slice
 		
-	// 	fmt.Print("Start: ",timeSlice.Start,"\n")
-	// 	fmt.Print("End: ",timeSlice.End)
-	// 	fmt.Print("\n\n")
-	// 	queryParams := tiledbgnss.ObsQueryParams{
-	// 		Time: []tiledbgnss.TimeRange{timeSlice},
-	// 	}
-	// 	epochs, err := tiledbgnss.ReadObsV3Array(
-	// 		*tdbPathPtr, "us-east-2", queryParams)
-	// 	if err != nil {
-	// 		fmt.Print("Error Reading TDB: ",err)
-	// 	}
-	// 	fmt.Print("Epochs: ",len(epochs),"\n")
-	// 	settings.TimeOfFirst = epochs[0].Time
-	// 	settings.TimeOfLast = epochs[len(epochs)-1].Time	
-	// 	err = processDailyEpoch(epochs, settings)
-	// 	epochs = nil // Clear the epochs list
-	// 	if err != nil {
-	// 		fmt.Print("Error Processing Daily Epoch: ",err)
-	// 	}
-	// 	fmt.Print("\n\n ===================== \n\n")
+		fmt.Print("Start: ",timeSlice.Start,"\n")
+		fmt.Print("End: ",timeSlice.End)
+		fmt.Print("\n\n")
+		queryParams := tiledbgnss.ObsQueryParams{
+			Time: []tiledbgnss.TimeRange{timeSlice},
+		}
+		epochs, err := tiledbgnss.ReadObsV3Array(
+			*tdbPathPtr, "us-east-2", queryParams)
+		if err != nil {
+			fmt.Print("Error Reading TDB: ",err)
+		}
+		fmt.Print("Epochs: ",len(epochs),"\n")
+		settings.TimeOfFirst = epochs[0].Time
+		settings.TimeOfLast = epochs[len(epochs)-1].Time	
+		err = processDailyEpoch(epochs, settings)
+		epochs = nil // Clear the epochs list
+		if err != nil {
+			fmt.Print("Error Processing Daily Epoch: ",err)
+		}
+		fmt.Print("\n\n ===================== \n\n")
 
-	//}
+	}
 }
 
-// func processDailyEpoch(epochs []observation.Epoch, settings *rinex.Settings) error {
-// 	// sort epochs by time
-// 	sort.Slice(epochs, func(i, j int) bool {
-// 		return epochs[i].Time.Before(epochs[j].Time)
-// 	})
+func processDailyEpoch(epochs []observation.Epoch, settings *rinex.Settings) error {
+	// sort epochs by time
+	sort.Slice(epochs, func(i, j int) bool {
+		return epochs[i].Time.Before(epochs[j].Time)
+	})
 
-// 	startYear,startMonth,startDay := epochs[0].Time.Date()
-// 	endYear,endMonth,endDay := epochs[len(epochs)-1].Time.Date()
+	startYear,startMonth,startDay := epochs[0].Time.Date()
+	endYear,endMonth,endDay := epochs[len(epochs)-1].Time.Date()
 
-// 	fmt.Print("Start Date: ",startYear,startMonth,startDay,"\n")
-// 	fmt.Print("End Date: ",endYear,endMonth,endDay)
-// 	fmt.Print("\n")
-// 	currentDate := time.Date(startYear,startMonth,startDay,0,0,0,0,time.UTC)
-// 	dayOfYear := currentDate.YearDay()
-// 	outFile := &os.File{}
-// 	yy := startYear % 100
-// 	filename := fmt.Sprintf("%s%03d0.%02do", settings.MarkerName, dayOfYear, yy)
-// 	fmt.Print("\nWriting to Filename: ",filename,"\n")
-// 	outFile, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0644)
+	fmt.Print("Start Date: ",startYear,startMonth,startDay,"\n")
+	fmt.Print("End Date: ",endYear,endMonth,endDay)
+	fmt.Print("\n")
+	currentDate := time.Date(startYear,startMonth,startDay,0,0,0,0,time.UTC)
+	dayOfYear := currentDate.YearDay()
+	outFile := &os.File{}
+	yy := startYear % 100
+	filename := fmt.Sprintf("%s%03d0.%02do", settings.MarkerName, dayOfYear, yy)
+	fmt.Print("\nWriting to Filename: ",filename,"\n")
+	outFile, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0644)
 
-// 	if err != nil {
-// 		return fmt.Errorf("failed creating output file: %s", err)
-// 	}
-// 	header, err := rinex.NewHeader(settings)
-// 	if err != nil {
-// 		return fmt.Errorf("failed creating RINEX header: %s", err)
-// 	}
-// 	err = header.Write(outFile)
-// 	if err != nil {
-// 		return fmt.Errorf("failed writing RINEX header: %s", err)
-// 	}
+	if err != nil {
+		return fmt.Errorf("failed creating output file: %s", err)
+	}
+	header, err := rinex.NewHeader(settings)
+	if err != nil {
+		return fmt.Errorf("failed creating RINEX header: %s", err)
+	}
+	err = header.Write(outFile)
+	if err != nil {
+		return fmt.Errorf("failed writing RINEX header: %s", err)
+	}
 
-// 	for _, epoch := range epochs {
-// 		if epoch.Time.Day() != currentDate.Day() {
-// 			// close current output file if it exists
-// 			log.Warnf("Detected Epoch Out of Range: %s > %s",epoch.Time,currentDate)
-// 			if outFile != nil {
-// 					err := outFile.Close()
-// 					if err != nil {
-// 						log.Warnf("failed closing file: %s", err)
-// 					}
-// 				}
-// 			break
+	for _, epoch := range epochs {
+		if epoch.Time.Day() != currentDate.Day() {
+			// close current output file if it exists
+			log.Warnf("Detected Epoch Out of Range: %s > %s",epoch.Time,currentDate)
+			if outFile != nil {
+					err := outFile.Close()
+					if err != nil {
+						log.Warnf("failed closing file: %s", err)
+					}
+				}
+			break
 
-// 		}
-// 		err = rinex.SerializeRnxObs(outFile, epoch, settings)
+		}
+		err = rinex.SerializeRnxObs(outFile, epoch, settings)
 
-// 		if err != nil {
-// 			log.Warnf("failed writing observation: %s", err)
-// 		}
+		if err != nil {
+			log.Warnf("failed writing observation: %s", err)
+		}
 
-// 	}
-// 	defer outFile.Close()
+	}
+	defer outFile.Close()
 	
-// 	return nil
-// }
+	return nil
+}
 
 // Helper function to parse metadata from the JSON file
 func parseSettings(path string) (*rinex.Settings, error) {
