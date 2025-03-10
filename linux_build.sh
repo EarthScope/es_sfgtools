f541b5b8d05c6d43ec7b38ff24e6c0da2458258b
frigusgulo <franklyn.dunbar@umontana.edu>
2025-03-10 13:24:44 -0600

    adj install script

--- linux_build.sh
+++ linux_build.sh
@@ -12,26 +12,13 @@ if ! command -v conda &> /dev/null; then
 fi
 conda env create -f linux_enviroment.yml
 
-# Stage 2: build golang binaries
-#SHELL ["conda", "run", "-n", "seafloor_geodesy_linux", "/bin/bash", "-c"]
 
 # build golang binaries
 cd src/golangtools
 
 # change shell to conda environment and run makefile
 CONDA_ENV_NAME="seafloor_geodesy_linux"
-# CONDA_ENV_PATH=$(conda env list | awk -v env_name="$CONDA_ENV_NAME" '$1 == env_name {print $NF}')
 
-# # Check if the environment path was found
-# if [ -z "$CONDA_ENV_PATH" ]; then
-#     echo "Conda environment '$CONDA_ENV_NAME' not found."
-#     exit 1
-# else
-#     echo "Conda environment '$CONDA_ENV_NAME' path: $CONDA_ENV_PATH"
-# fi
-
-# # run makefile
-# $CONDA_ENV_PATH/bin/bash run -n
 conda activate $CONDA_ENV_NAME
 make -B 
 
