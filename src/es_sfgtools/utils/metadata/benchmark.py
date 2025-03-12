867d0ada984e353688297900996cf0b19779702d
Rachel Terry <rachel.terry@earthscope.org>
2025-03-11 22:11:51 -0600

    initial pydantic changes for benchmark and campaign

--- src/es_sfgtools/utils/metadata/benchmark.py
+++ src/es_sfgtools/utils/metadata/benchmark.py
@@ -1,143 +1,35 @@
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 from datetime import datetime
+from pydantic import BaseModel, Field, field_validator
 
-from es_sfgtools.utils.metadata.utils import AttributeUpdater, Location, convert_to_datetime
+from es_sfgtools.utils.metadata.utils import AttributeUpdater, Location,  parse_datetime, check_dates
+
+class Transponder(AttributeUpdater, BaseModel):
+    # Required
+    address: str
+    tat: float
+    start: datetime
+
+    # Optional
+    end: Optional[datetime] = Field(default=None)
+    uid: Optional[str] = Field(default=None)
+    model: Optional[str] = Field(default=None)
+    serialNumber: Optional[str] = Field(default=None)
+    batteryCapacity: Optional[str] = Field(default=None)
+    notes: Optional[str] = Field(default=None)
+    batteryVoltage: Optional[List[Any]] = Field(default=None)
+    extraSensors: Optional[List[Any]] = Field(default=None)
 
 
+class Benchmark(AttributeUpdater, BaseModel):
+    # Required
+    name: str
+    benchmarkID: str
+    dropPointLocation: Location
+    aPrioriLocation: Optional[Location] = Field(default=None)
+    start: datetime
+    end: Optional[datetime] = Field(default=None)
+    transponders: List[Transponder] = Field(default_factory=list)
 
-class Benchmark(AttributeUpdater):
-    def __init__(self, name: str = None, additional_data: Dict[str, Any] = None, existing_benchmark: Dict[str, Any] = None):
-
-        if existing_benchmark:
-            self.import_exisiting_benchmark(existing_benchmark)
-            return
-        
-        self.name: str = name
-        self.benchmarkID: str = ""  
-        self.dropPointLocation = Location()
-        self.aPrioriLocation = Location()
-        self.start: datetime = None
-        self.end: datetime = None
-        self.transponders: List[Transponder] = []
-
-        if additional_data:
-            self.update_attributes(additional_data)
-
-    def import_exisiting_benchmark(self, existing_benchmark: Dict[str, Any]):
-        """
-        Import an existing benchmark from a dictionary.
-
-        Args:
-            existing_benchmark (Dict[str, Any]): A dictionary containing the existing benchmark data.
-        """
-        self.name = existing_benchmark.get("name")  # Required
-        self.benchmarkID = existing_benchmark.get("benchmarkID", "")
-
-        drop_point_location_data = existing_benchmark.get("dropPointLocation", {})
-        if isinstance(drop_point_location_data, dict):
-            self.dropPointLocation = Location(additional_data=drop_point_location_data)
-    
-        a_priori_location_data = existing_benchmark.get("aPrioriLocation", {})
-        if isinstance(a_priori_location_data, dict):
-            self.aPrioriLocation = Location(additional_data=a_priori_location_data)
-
-        start_time = existing_benchmark.get("start", "")
-        if start_time:
-            self.start = convert_to_datetime(start_time)
-        else:
-            self.start = None
-
-        end_time = existing_benchmark.get("end", "")
-        if end_time:
-            self.end = convert_to_datetime(end_time)
-        else:
-            self.end = None
-
-        self.transponders = [Transponder(existing_transponder=tran) for tran in existing_benchmark.get("transponders", [])]
-
-    def to_dict(self) -> Dict[str, Any]:
-        """
-        Convert the Benchmark instance to a dictionary.
-
-        Returns:
-            Dict[str, Any]: A dictionary representation of the Benchmark instance.
-        """
-        return {
-            "name": self.name,
-            "benchmarkID": self.benchmarkID if self.benchmarkID else "",
-            "dropPointLocation": self.dropPointLocation.to_dict() if self.dropPointLocation else {}, 
-            "aPrioriLocation": self.aPrioriLocation.to_dict() if self.aPrioriLocation else {},
-            "start": self.start.strftime('%Y-%m-%dT%H:%M:%S') if self.start else "",
-            "end": self.end.strftime('%Y-%m-%dT%H:%M:%S') if self.end else "",
-            "transponders": [transponder.to_dict() for transponder in self.transponders]
-        }
-    
-class Transponder(AttributeUpdater):
-    def __init__(self, address: str = None, additional_data: Dict[str, Any] = None, existing_transponder: Dict[str, Any] = None):
-        if existing_transponder:
-            self.import_existing_transponder(existing_transponder)
-            return
-        
-        self.address: str = address
-        self.uid: str = ""
-        self.model: str = ""
-        self.serialNumber: str = ""
-        self.batteryCapacity: str = ""
-        self.tat: float = 0.0
-        self.start: datetime = None
-        self.end: datetime = None
-        self.notes: str = ""
-        self.batteryVoltage: List[Any] = []
-        self.extraSensors: List[Any]  = []
-
-        if additional_data:
-            self.update_attributes(additional_data)
-
-    def import_existing_transponder(self, existing_transponder: Dict[str, Any]):
-        """
-        Import an existing transponder from a dictionary.
-        """
-        self.address = existing_transponder.get("address", "")
-        self.uid = existing_transponder.get("uid", "")
-        self.model = existing_transponder.get("model", "")
-        self.serialNumber = existing_transponder.get("serialNumber", "")    
-        self.batteryCapacity = existing_transponder.get("batteryCapacity", "")
-        self.tat = existing_transponder.get("tat", 0.0)
-
-        start_time = existing_transponder.get("start", "")
-        if start_time:
-            self.start = convert_to_datetime(start_time)
-        else:
-            self.start = None
-
-        end_time = existing_transponder.get("end", "")
-        if end_time:
-            self.end = convert_to_datetime(end_time)
-        else:
-            self.end = None
-
-        self.notes = existing_transponder.get("notes", "")
-        self.batteryVoltage = existing_transponder.get("batteryVoltage", [])
-        self.extraSensors = existing_transponder.get("extraSensors", [])
-
-    def to_dict(self) -> Dict[str, Any]:
-        """
-        Convert the Transponder instance to a dictionary.
-
-        Returns:
-            Dict[str, Any]: A dictionary representation of the Transponder instance.
-        """
-        return {
-            "address": str(self.address),
-            "uid": str(self.uid) if self.uid else "",
-            "model": str(self.model) if self.model else "",
-            "serialNumber": str(self.serialNumber) if self.serialNumber else "",
-            "batteryCapacity": str(self.batteryCapacity) if self.batteryCapacity is not None else "",
-            "tat": float(self.tat) if self.tat is not None else 0.0,
-            "start": self.start.strftime('%Y-%m-%dT%H:%M:%S') if self.start else "",
-            "end": self.end.strftime('%Y-%m-%dT%H:%M:%S') if self.end else "",
-            "notes": str(self.notes) if self.notes else "",
-            "batteryVoltage": self.batteryVoltage,
-            "extraSensors": self.extraSensors
-        }
-
+    _parse_datetime = field_validator('start', 'end', mode='before')(parse_datetime)
+    _check_dates = field_validator('end')(check_dates)
--- src/es_sfgtools/utils/metadata/campaign.py
+++ src/es_sfgtools/utils/metadata/campaign.py
@@ -1,7 +1,9 @@
 from datetime import datetime
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
-from es_sfgtools.utils.metadata.utils import AttributeUpdater, convert_to_datetime
+from es_sfgtools.utils.metadata.utils import AttributeUpdater, check_dates, parse_datetime
+from pydantic import BaseModel, Field, field_validator
+
 
 def campaign_checks(campaign_year, campaign_interval, vessel_code):
     """ Check the campaign year, interval and vessel code 
@@ -28,134 +30,38 @@ def campaign_checks(campaign_year, campaign_interval, vessel_code):
     return campaign_name,  vessel_code.upper()
 
 
-class Survey(AttributeUpdater):
-    def __init__(self, survey_id: str= None, additional_data: Dict[str, Any] = None, existing_survey: Dict[str, Any] = None):
-        if existing_survey:
-            self.import_existing_survey(existing_survey)
-            return
-        
-        self.survey_id = survey_id
-        self.type: str = ""
-        self.benchmarkIDs: List[str] = []
-        self.start: datetime = None
-        self.end: datetime = None
-        self.notes: str = ""
-        self.commands: str = ""
+class Survey(AttributeUpdater, BaseModel):
+    # Required
+    id: str
+    type: str
+    benchmarkIDs: List[str]
+    start: datetime
 
-        if additional_data:
-            self.update_attributes(additional_data)
+    # Optional
+    end: Optional[datetime] = Field(default=None)
+    notes: Optional[str] = Field(default=None)
+    commands: Optional[str] = Field(default=None)
 
-    def import_existing_survey(self, existing_survey: Dict[str, Any]):
-        """
-        Import an existing survey from a dictionary.
-
-        Args:
-            existing_survey (Dict[str, Any]): A dictionary containing the existing survey data.
-        """
-        self.survey_id = existing_survey.get("id", "")
-        self.type = existing_survey.get("type", "")
-        self.benchmarkIDs = existing_survey.get("benchmarkIDs", [])
-        start_time = existing_survey.get("start", "")
-        if start_time:
-            self.start = convert_to_datetime(start_time)
-        else:
-            self.start = None
-
-        end_time = existing_survey.get("end", "")
-        if end_time:
-            self.end = convert_to_datetime(end_time)
-        else:
-            self.end = None
-
-        self.notes = existing_survey.get("notes", "")
-        self.commands = existing_survey.get("commands", "")
-
-    def to_dict(self) -> Dict[str, Any]:
-        """
-        Convert the Survey instance to a dictionary.
-
-        Returns:
-            Dict[str, Any]: A dictionary representation of the Survey instance.
-        """
-        return {
-            "id": self.survey_id,
-            "type": self.type if self.type else "",
-            "benchmarkIDs": self.benchmarkIDs if self.benchmarkIDs else [],
-            "start": self.start.strftime('%Y-%m-%dT%H:%M:%S') if self.start else "",
-            "end": self.end.strftime('%Y-%m-%dT%H:%M:%S') if self.end else "",
-            "notes": self.notes if self.notes else "",
-            "commands": self.commands if self.commands else ""
-        }
+    _parse_datetime = field_validator('start', 'end', mode='before')(parse_datetime)
+    _check_dates = field_validator('end', mode='after')(check_dates)
 
 
-class Campaign(AttributeUpdater):
+class Campaign(AttributeUpdater, BaseModel):
+    # Required
+    name: str
+    type: str
+    vesselCode: str
+    start: datetime
+    end: datetime
 
-    def __init__(self, name: str = None, additional_data: Dict[str, Any] = None, existing_campaign: dict = None):
-        self.vesselCode: str = ""
-        self.type: str = ""
-        self.principalInvestigator: str = ""
-        self.launchVesselName: str = ""
-        self.recoveryVesselName: str = ""
-        self.cruiseName: str = ""
-        self.technicianName: str = ""
-        self.technicianContact: str = ""
-        self.start: datetime = None
-        self.end: datetime = None
-        self.surveys: List[Survey] = []
-        
-        if existing_campaign:
-            self.import_existing_campaign(existing_campaign)
-            return 
-        
-        self.name = name
+    # Optional
+    principalInvestigator: Optional[str] = Field(default=None)
+    launchVesselName: Optional[str] = Field(default=None)
+    recoveryVesselName: Optional[str] = Field(default=None)
+    cruiseName: Optional[str] = Field(default=None)
+    technicianName: Optional[str] = Field(default=None)
+    technicianContact: Optional[str] = Field(default=None)
+    surveys: List[Survey] = Field(default_factory=list)
 
-        if additional_data:
-            self.update_attributes(additional_data)
-
-    def import_existing_campaign(self, existing_campaign: dict):
-        self.name = existing_campaign.get("name", "")
-        self.vesselCode = existing_campaign.get("vesselCode", "")
-        self.type = existing_campaign.get("type", "")
-        self.principalInvestigator = existing_campaign.get("principalInvestigator", "")
-        self.launchVesselName = existing_campaign.get("launchVesselName", "")
-        self.recoveryVesselName = existing_campaign.get("recoveryVesselName", "")
-        self.cruiseName = existing_campaign.get("cruiseName", "")
-        self.technicianName = existing_campaign.get("technicianName", "")
-        self.technicianContact = existing_campaign.get("technicianContact", "")
-
-        start = existing_campaign.get("start", "")
-        if start:
-            self.start = convert_to_datetime(start)
-        else:
-            self.start = None
-
-        end = existing_campaign.get("end", "")
-        if end:
-            self.end = convert_to_datetime(end)
-        else:
-            self.end = None
-
-        self.surveys = [Survey(survey_id=survey["id"], existing_survey=survey ) for survey in existing_campaign["surveys"]]
-
-
-    def to_dict(self) -> Dict[str, Any]:
-        """
-        Convert the Campaign instance to a dictionary.
-
-        Returns:
-            Dict[str, Any]: A dictionary representation of the Campaign instance.
-        """
-        return {
-            "name": self.name,
-            "vesselCode": self.vesselCode,
-            "type": self.type if self.type else "",
-            "launchVesselName": self.launchVesselName if self.launchVesselName else "",
-            "recoveryVesselName": self.recoveryVesselName if self.recoveryVesselName else "",
-            "cruiseName": self.cruiseName if self.cruiseName else "",
-            "technicianName": self.technicianName if self.technicianName else "",
-            "technicianContact": self.technicianContact if self.technicianContact else "",
-            "start": self.start.strftime('%Y-%m-%dT%H:%M:%S') if self.start else "",
-            "end": self.end.strftime('%Y-%m-%dT%H:%M:%S') if self.end else "",
-            "surveys": [survey.to_dict() for survey in self.surveys]    
-        }
-
+    _parse_datetime = field_validator('start', 'end', mode='before')(parse_datetime)
+    _check_dates = field_validator('end', mode='after')(check_dates)
