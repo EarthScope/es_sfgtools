29077701d73b18969abc569f1ddb2f4dd5e22446
Franklyn Dunbar <franklyn.dunbar@earthscope.org>
2025-03-24 13:59:43 -0600

    docs

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from es_sfgtools.processing.pipeline.pipelines import (
     PipelineManifest,
 )
 from es_sfgtools.processing.pipeline.data_handler import DataHandler
-
+from es_sfgtools.utils.archive_pull import list_survey_files
 pride_path = Path.home() / ".PRIDE_PPPAR_BIN"
 os.environ["PATH"] += os.pathsep + str(pride_path)
 
@@ -24,6 +24,14 @@ def run(file:Path):
         )
         assert ingest_job.directory.exists(), "Directory listed does not exist"
         dh.discover_data_and_add_files(ingest_job.directory)
+
+    for job in manifest_object.download_jobs:
+        urls = list_survey_files(**job.model_dump())
+        if not urls:
+            print(f"No Remote Assets Found For {job.model_dump()}")
+        dh.change_working_station(**job.model_dump())
+        dh.add_data_remote(remote_filepaths=urls)
+        dh.download_data()
 
     for job in manifest_object.process_jobs:
         dh.change_working_station(
