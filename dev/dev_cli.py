f86a6cca79486969fe0063db3b606398d2e53599
Rachel Terry <rachel.terry@earthscope.org>
2025-04-14 12:21:05 -0600

    formatting, survey to campaign

--- dev/dev_cli.py
+++ dev/dev_cli.py
@@ -1,8 +1,13 @@
-from es_sfgtools.processing.pipeline.pipelines import SV3PipelineConfig,PipelineManifest,SV3Pipeline
-from es_sfgtools.utils.archive_pull import list_survey_files
+from es_sfgtools.processing.pipeline.pipelines import (
+    SV3PipelineConfig,
+    PipelineManifest,
+    SV3Pipeline,
+)
+from es_sfgtools.utils.archive_pull import list_campaign_files
 from pathlib import Path
 from es_sfgtools.processing.pipeline.data_handler import DataHandler
 import os
+
 pride_path = Path.home() / ".PRIDE_PPPAR_BIN"
 os.environ["PATH"] += os.pathsep + str(pride_path)
 
@@ -32,21 +37,27 @@ if __name__ == "__main__":
 
     dh = DataHandler(manifest_object.main_dir)
     for ingest_job in manifest_object.ingestion_jobs:
-        dh.change_working_station(network=ingest_job.network,station=ingest_job.station,campaign=ingest_job.campaign)
+        dh.change_working_station(
+            network=ingest_job.network,
+            station=ingest_job.station,
+            campaign=ingest_job.campaign,
+        )
         assert ingest_job.directory.exists(), "Directory listed does not exist"
         dh.discover_data_and_add_files(ingest_job.directory)
 
     for job in manifest_object.download_jobs:
-        urls = list_survey_files(**job.model_dump())
+        urls = list_campaign_files(**job.model_dump())
         if not urls:
             print(f"No Remote Assets Found For {job.model_dump()}")
         dh.change_working_station(**job.model_dump())
         dh.add_data_remote(remote_filepaths=urls)
         dh.download_data()
-        
+
     for job in manifest_object.process_jobs:
-        dh.change_working_station(network=job.network,station=job.station,campaign=job.campaign)
-        pipeline,config = dh.get_pipeline_sv3()
+        dh.change_working_station(
+            network=job.network, station=job.station, campaign=job.campaign
+        )
+        pipeline, config = dh.get_pipeline_sv3()
         job.config.rinex_config.settings_path = dh.rinex_metav2
         pipeline.config = job.config
         pipeline.run_pipeline()
