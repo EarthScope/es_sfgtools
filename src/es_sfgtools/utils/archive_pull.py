fcfc3f2816138265c04edbf07b98195e7c20a47b
Rachel Terry <rachel.terry@earthscope.org>
2025-04-14 12:20:34 -0600

    BREAKING CHANGE - survey to campaign (also black formatting)

--- src/es_sfgtools/utils/archive_pull.py
+++ src/es_sfgtools/utils/archive_pull.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import os
 import urllib.request
 import ssl
-import logging
 from typing import List
 import boto3
 
@@ -12,6 +11,7 @@ ssl._create_default_https_context = ssl._create_stdlib_context
 from earthscope_sdk.auth.device_code_flow import DeviceCodeFlowSimple
 from earthscope_sdk.auth.auth_flow import NoTokensError
 from es_sfgtools.utils.loggers import ProcessLogger as logger
+
 
 def retrieve_token(token_path="."):
     """
@@ -33,11 +33,11 @@ def retrieve_token(token_path="."):
 
     return token
 
-def download_file_from_archive(url, 
-                               dest_dir='./', 
-                               token_path=".",
-                               show_details: bool=True) -> None:
-    """ 
+
+def download_file_from_archive(
+    url, dest_dir="./", token_path=".", show_details: bool = True
+) -> None:
+    """
     Download a file from the public archive using the EarthScope SDK.
     Args:
         url (str): The URL of the file to download.
@@ -45,9 +45,9 @@ def download_file_from_archive(url,
         token_path (str): The path to the token file.
         logger (logging.Logger): The logger to use
         show_details (bool): log the file details
-    """ 
+    """
 
-    # Make the destination directory if it doesn't exist
+    # Make the destination directory if it doesn't exis
     if not os.path.exists(dest_dir):
         os.makedirs(dest_dir)
 
@@ -62,11 +62,13 @@ def download_file_from_archive(url,
     if r.status_code == requests.codes.ok:
         if show_details:
             logger.loginfo(f"Downloading {url} to {destination_file}")
-        with open(destination_file, 'wb') as f:
+        with open(destination_file, "wb") as f:
             for data in r:
                 f.write(data)
     else:
-        raise Exception(f"Failed to download file from {url}, status code: {r.status_code}, reason: {r.reason}")
+        raise Exception(
+            f"Failed to download file from {url}, status code: {r.status_code}, reason: {r.reason}"
+        )
 
 
 def list_files_from_archive(url, token_path=".") -> list:
@@ -81,23 +83,26 @@ def list_files_from_archive(url, token_path=".") -> list:
     token = retrieve_token(token_path)
 
     # Generate the list URL
-    list_url = os.path.join(url, "?list&uris=1")    # ?list returns a list of files and &uris=1 returns the full path
+    list_url = os.path.join(
+        url, "?list&uris=1"
+    )  # ?list returns a list of files and &uris=1 returns the full path
 
     # Create a request object
     req = urllib.request.Request(list_url)
 
     # Add the authorization header
-    req.add_header('Authorization', 'Bearer ' + token)
+    req.add_header("Authorization", "Bearer " + token)
 
     try:
         # Send the request and capture the response
         with urllib.request.urlopen(req) as response:
-            result = response.read().decode('utf-8')
+            result = response.read().decode("utf-8")
     except Exception as e:
         logger.logerr(e)
         return []
 
     return _parse_output(result)
+
 
 def _parse_output(output) -> list:
     """
@@ -109,19 +114,21 @@ def _parse_output(output) -> list:
         list: A list of files
     """
 
-    lines = output.split('\n')
+    lines = output.split("\n")
     files = []
 
     for line in lines:
-        if not line.strip():    # Skip empty lines
+        if not line.strip():  # Skip empty lines
             continue
-        
+
         files.append(line)
 
     return files
 
 
-def download_file_list_from_archive(file_urls: list, dest_dir='./files', token_path=".") -> None:
+def download_file_list_from_archive(
+    file_urls: list, dest_dir="./files", token_path="."
+) -> None:
     """
     Download a list of files from the public archive
 
@@ -133,13 +140,13 @@ def download_file_list_from_archive(file_urls: list, dest_dir='./files', token_p
 
     successful_files = []
     failed_files = []
-    
+
     logger.loginfo(f"Downloading {len(file_urls)} files to {dest_dir}")
     for url in file_urls:
-        try: 
-            download_file_from_archive(url=url, 
-                                    dest_dir=dest_dir, 
-                                    token_path=token_path)
+        try:
+            download_file_from_archive(
+                url=url, dest_dir=dest_dir, token_path=token_path
+            )
             successful_files.append(url)
         except Exception as e:
             logger.logerr(f"Failed to download {url}: {e}")
@@ -150,71 +157,66 @@ def download_file_list_from_archive(file_urls: list, dest_dir='./files', token_p
         logger.logwarn(f"Failed to download {len(failed_files)} files.")
         print(failed_files)
 
-def generate_archive_survey_url(network, station, survey):
+
+def generate_archive_campaign_url(network, station, campaign):
     """
-    Generate a URL for a survey in the public archive
+    Generate a URL for a campaign in the public archive
 
     Args:
         network (str): The network name
         station (str): The station name
-        survey (str): The survey name
+        campaign (str): The campaign name
 
     Returns:
-        str: The URL of the survey directory
-    """ 
+        str: The URL of the campaign directory
+    """
 
-    return f'https://gage-data.earthscope.org/archive/seafloor/{network}/{station}/{survey}/raw'
+    return f"https://gage-data.earthscope.org/archive/seafloor/{network}/{station}/{campaign}/raw"
 
-def list_file_counts_by_type(file_list: list, url: str=None, show_details: bool=True) -> dict:
+
+def list_file_counts_by_type(file_list: list, url: str = None) -> dict:
     """
     Counts files by type, and builds a dictionary.
 
     Args:
         file_list (list): list of files from the archive
         url (str): url of where in the archive the files were found
-        show_details (bool): log details
 
     Returns:
         dict: dictionary of files by type
     """
     file_dict = {}
     for file in file_list:
-        if 'master' in file:
-            file_dict.setdefault('master', []).append(file)
-        elif 'lever_arms' in file:
-            file_dict.setdefault('lever_arms', []).append(file)
-        elif 'bcsonardyne' in file:
-            file_dict.setdefault('sonardyne', []).append(file)
-        elif 'bcnovatel' in file:
-            file_dict.setdefault('novatel', []).append(file)
-        elif 'bcoffload' in file:
-            file_dict.setdefault('offload', []).append(file)
-        elif file.endswith('NOV770.raw'):
-            file_dict.setdefault('NOV770', []).append(file)
-        elif file.endswith('DFOP00.raw'):
-            file_dict.setdefault('DFOP00', []).append(file)
-        elif file.endswith('NOV000.bin'):
-            file_dict.setdefault('NOV000', []).append(file)
+        if "master" in file:
+            file_dict.setdefault("master", []).append(file)
+        elif "lever_arms" in file:
+            file_dict.setdefault("lever_arms", []).append(file)
+        elif "bcsonardyne" in file:
+            file_dict.setdefault("sonardyne", []).append(file)
+        elif "bcnovatel" in file:
+            file_dict.setdefault("novatel", []).append(file)
+        elif "bcoffload" in file:
+            file_dict.setdefault("offload", []).append(file)
+        elif file.endswith("NOV770.raw"):
+            file_dict.setdefault("NOV770", []).append(file)
+        elif file.endswith("DFOP00.raw"):
+            file_dict.setdefault("DFOP00", []).append(file)
+        elif file.endswith("NOV000.bin"):
+            file_dict.setdefault("NOV000", []).append(file)
         elif "ctd" in file:
-            file_dict.setdefault('ctd', []).append(file)
+            file_dict.setdefault("ctd", []).append(file)
 
     if url is not None:
-        logger.loginfo(f'Found under {url}:')
+        logger.loginfo(f"Found under {url}:")
     else:
-        logger.loginfo('Found:')
-    for k,v in file_dict.items():
-        logger.loginfo(f'    {len(v)} {k} file(s)')
+        logger.loginfo("Found:")
+    for k, v in file_dict.items():
+        logger.loginfo(f"    {len(v)} {k} file(s)")
 
-    # if show_details:
-    #     if url is not None:
-    #         print(f'Found under {url}:')
-    #     else:
-    #         print('Found:')
-    #     for k,v in file_dict.items():
-    #         print(f'    {len(v)} {k} file(s)')
-    return file_dict  
+    return file_dict
 
-def get_survey_file_dict(url:str) -> dict:
+
+def get_survey_file_dict(url: str) -> dict:
     """gets
 
     :param url: location in archive
@@ -222,62 +224,62 @@ def get_survey_file_dict(url:str) -> dict:
     :return: dictionary of file locations by type
     :rtype: dict
     """
-    
+
     file_list = list_files_from_archive(url)
     return list_file_counts_by_type(file_list, url)
 
-def list_survey_files(network: str, 
-                      station: str, 
-                      campaign: str,
-                      show_details: bool=True) -> list:
+
+def list_campaign_files(network: str, station: str, campaign: str) -> list:
     """
-    Returns a list of files for a given survey in the archive.  Optionally displays a summary of file counts by type
+    Returns a list of files for a given campaign in the archive.  Optionally displays a summary of file counts by type
 
     Args:
         network (str): network name
         station (str): station name
-        survey (str): survey name
-        show_details (bool): option to display summary of file counts by type, defaults to True
+        campaign (str): campaign name
 
     Returns:
         list: list of file locations in archive
     """
 
-    url = generate_archive_survey_url(network, station, campaign)
-    logger.loginfo(f"Listing survey files from url {url}")
+    url = generate_archive_campaign_url(network, station, campaign)
+    logger.loginfo(f"Listing campaign files from url {url}")
     file_list = list_files_from_archive(url)
-    file_list += list_files_from_archive(f'{url}/ctd')
-    list_file_counts_by_type(file_list=file_list, url=url, show_details=show_details)
+    file_list += list_files_from_archive(f"{url}/ctd")
+    list_file_counts_by_type(file_list=file_list, url=url)
 
-    return file_list  
+    return file_list
 
 
-def list_s3_directory_files(bucket_name:str, prefix:str) -> List[str]:
+def list_s3_directory_files(bucket_name: str, prefix: str) -> List[str]:
     """
     Returns a list all files in a given S3 bucket under a specified prefix and return absolute S3 paths.
 
-    Args: 
+    Args:
         bucket_name (str): Name of the S3 bucket.
         prefix (str): S3 prefix (folder path) to filter the files.
-    Returns: 
+    Returns:
         List[str]: List of absolute S3 file paths.
     """
     s3_client = boto3.client("s3")
     file_paths = []
-    
+
     paginator = s3_client.get_paginator("list_objects_v2")
     for page in paginator.paginate(Bucket=bucket_name, Prefix=prefix):
         if "Contents" in page:
-            file_paths.extend(f"s3://{bucket_name}/{obj['Key']}" for obj in page["Contents"])
-    
+            file_paths.extend(
+                f"s3://{bucket_name}/{obj['Key']}" for obj in page["Contents"]
+            )
+
     return file_paths
 
-if __name__ == "__main__":    
-    # Example usage 
+
+if __name__ == "__main__":
+    # Example usage
 
     # Download file from public arhive
     url = "https://gage-data.earthscope.org/archive/gnss/L1/rinex/1998/300/cal13000.98S"
-    download_file_from_archive(url, dest_dir='./files')
+    download_file_from_archive(url, dest_dir="./files")
 
     # List files from public arhive
     url = "https://gage-data.earthscope.org/archive/gnss/rinex/met/2021/072"
