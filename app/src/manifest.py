b469e84478ca5e3e4de65b4b94edc1004ffe4e31
Franklyn Dunbar <franklyn.dunbar@earthscope.org>
2025-05-01 17:12:53 -0600

    add yaml

--- app/src/manifest.py
+++ app/src/manifest.py
@@ -62,6 +62,74 @@ class PipelineManifest(BaseModel):
         arbitrary_types_allowed = True
 
     @classmethod
+    def _load(cls,data:dict) -> 'PipelineManifest':
+        global_config = SV3PipelineConfig(**data["globalConfig"])
+
+        # Initialize lists for jobs
+        process_jobs = []
+        ingestion_jobs = []
+        download_jobs = []
+
+        # Parse operations
+        for operation in data.get("operations", []):
+            # Extract network, station, and campaign
+            try:
+                network = operation["network"]
+                station = operation["station"]
+                campaign = operation["campaign"]
+            except KeyError as e:
+                raise ValueError(f"Missing key in operations: {e}")
+
+            for job in operation.get("jobs", []):
+                # Validate job type
+                try:
+                    job_type = PipelineJobType(job["type"])
+                except (KeyError, ValueError) as e:
+                    raise ValueError(f"Invalid job type: {job['type']}") from e
+
+                match job_type:
+                    case PipelineJobType.INGESTION:
+                        ingestion_jobs.append(
+                            PipelineIngestJob(
+                                network=network,
+                                station=station,
+                                campaign=campaign,
+                                directory=job["directory"],  # need to validate this
+                            )
+                        )
+                    case PipelineJobType.PREPROCESSING:
+                        # Merge job-specific config with global config
+                        job_config = (
+                            SV3PipelineConfig(**job["config"])
+                            if "config" in job
+                            else global_config
+                        )
+                        job_config = global_config.model_copy(update=dict(job_config))
+                        process_jobs.append(
+                            PipelinePreprocessJob(
+                                network=network,
+                                station=station,
+                                campaign=campaign,
+                                config=job_config,
+                            )
+                        )
+                    case PipelineJobType.DOWNLOAD:
+                        download_jobs.append(
+                            ArchiveDownloadJob(
+                                network=network, station=station, campaign=campaign
+                            )
+                        )
+
+        # Instantiate the PipelineManifest
+        return cls(
+            main_dir=Path(data["projectDir"]),
+            ingestion_jobs=ingestion_jobs,
+            process_jobs=process_jobs,
+            download_jobs=download_jobs,
+            global_config=global_config,
+        )
+    
+    @classmethod
     def from_json(cls, json_data:Path) -> 'PipelineManifest':
         """
         Instantiates a PipelineManifest object from a JSON schema.
@@ -75,67 +143,20 @@ class PipelineManifest(BaseModel):
         # Load JSON data
         with open(json_data, "r") as f:
             json_data = json.load(f)
-        # Load global configuration
-        global_config = SV3PipelineConfig(**json_data["globalConfig"])
+        return cls._load(json_data)
+    
+    @classmethod
+    def from_yaml(cls, yaml_data:Path) -> 'PipelineManifest':
+        """
+        Instantiates a PipelineManifest object from a YAML schema.
 
-        # Initialize lists for jobs
-        process_jobs = []
-        ingestion_jobs = []
-        download_jobs = []
+        Args:
+            yaml_data (dict): The YAML data to parse.
 
-        # Parse operations
-        for operation in json_data.get("operations", []):
-            # Extract network, station, and campaign
-            try:
-                network = operation["network"]
-                station = operation["station"]
-                campaign = operation["campaign"]
-            except KeyError as e:
-                raise ValueError(f"Missing key in operations: {e}")     
-                   
-            for job in operation.get("jobs", []):
-                # Validate job type
-                try:
-                    job_type = PipelineJobType(job["type"])
-                except (KeyError,ValueError) as e:
-                    raise ValueError(f"Invalid job type: {job['type']}") from e
-                
-                match job_type:
-                    case PipelineJobType.INGESTION:
-                        ingestion_jobs.append(
-                            PipelineIngestJob(
-                                network=network,
-                                station=station,
-                                campaign=campaign,
-                                directory=job["directory"] # need to validate this
-                            )
-                        )
-                    case PipelineJobType.PREPROCESSING:
-                        # Merge job-specific config with global config
-                        job_config = SV3PipelineConfig(**job["config"]) if "config" in job else global_config
-                        job_config = global_config.model_copy(update=dict(job_config))
-                        process_jobs.append(
-                            PipelinePreprocessJob(
-                                network=network,
-                                station=station,
-                                campaign=campaign,
-                                config=job_config
-                            )
-                        )
-                    case PipelineJobType.DOWNLOAD:
-                        download_jobs.append(
-                            ArchiveDownloadJob(
-                                network=network,
-                                station=station,
-                                campaign=campaign
-                            )
-                        )
-
-        # Instantiate the PipelineManifest
-        return cls(
-            main_dir=Path(json_data["projectDir"]),
-            ingestion_jobs=ingestion_jobs,
-            process_jobs=process_jobs,
-            download_jobs=download_jobs,
-            global_config=global_config
-        )
+        Returns:
+            PipelineManifest: An instance of the PipelineManifest class.
+        """
+        # Load YAML data
+        with open(yaml_data, "r") as f:
+            yaml_data = yaml.safe_load(f)
+        return cls._load(yaml_data)
