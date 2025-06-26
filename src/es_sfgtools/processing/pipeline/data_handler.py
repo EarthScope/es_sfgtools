97c288903ae37b482abc1c3e8bd1902e89b8fa0d
Rachel Terry <rachel.terry@earthscope.org>
2025-06-26 13:36:47 -0600

    change to set env in code

--- src/es_sfgtools/processing/pipeline/data_handler.py
+++ src/es_sfgtools/processing/pipeline/data_handler.py
@@ -1,4 +1,5 @@
 """ Contains the DataHandler class for handling data operations. """
+import os
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -316,7 +317,7 @@ class DataHandler:
         change_all_logger_dirs(self.station_log_dir)
 
         logger.loginfo(f"Changed working station to {network} {station}")
-        return self.station_log_dir
+        os.environ["LOG_FILE_PATH"] = str(self.station_log_dir)
 
     @check_network_station_campaign
     def get_dtype_counts(self):
