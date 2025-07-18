2078d41c5fd4127aa7e4816955703f5423b76306
Franklyn Dunbar <franklyn.dunbar@earthscope.org>
2025-07-17 22:22:08 -0600

    chg logger

--- src/es_sfgtools/data_mgmt/utils.py
+++ src/es_sfgtools/data_mgmt/utils.py
@@ -15,7 +15,7 @@ from sklearn.preprocessing import MaxAbsScaler
 
 # Local imports
 from ..tiledb_tools.tiledb_schemas import TDBGNSSArray,TDBShotDataArray
-from ..logging import PRIDELogger as logger
+from ..logging import ProcessLogger as logger
 
 
 def interpolate_enu(
