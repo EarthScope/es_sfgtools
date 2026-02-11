9933996b410c44e00e7bf431a18693240be17da2
Rachel Terry <rachel.terry@earthscope.org>
2026-02-11 16:04:13 -0700

    create asset catalog regardless of env.

--- src/es_sfgtools/workflows/utils/protocols.py
+++ src/es_sfgtools/workflows/utils/protocols.py
@@ -166,10 +166,8 @@ class WorkflowABC(ABC):
             directory_handler.build()
 
         if asset_catalog is None:
-            if Environment.working_environment() == WorkingEnvironment.LOCAL:
-                asset_catalog = PreProcessCatalogHandler(db_path=directory_handler.asset_catalog_db_path)
-            else:
-                asset_catalog = None
+            asset_catalog = PreProcessCatalogHandler(db_path=directory_handler.asset_catalog_db_path)
+            
         if directory is None:
             directory = directory_handler.location
 
